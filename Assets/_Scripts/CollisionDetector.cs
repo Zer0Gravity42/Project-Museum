7a5e1219e68455efe06a4ffc4f062dd7e05dffd2
Lucy Herbold <xth4714@g.rit.edu>
2024-09-20 15:41:46 -0400

    fixed collisions
    Separated collision manager into two parts, "OnTriggers" and "OnCollisions"

--- Assets/_Scripts/CollisionDetector.cs
+++ Assets/_Scripts/CollisionDetector.cs
@@ -6,7 +6,7 @@ public class CollisionDetector : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "SpectralEnemy")
         {
             if (tag == "Weapon")
             {
@@ -25,6 +25,38 @@ public class CollisionDetector : MonoBehaviour
     }
 
     private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "SpectralEnemy")
+        {
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.white; // Change to default or previous color
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if (tag == "Weapon")
+            {
+                collision.gameObject.GetComponent<Enemy>().takeDamage(1);
+                collision.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+            if (collision.gameObject.tag == "Player")
+            {
+                if (tag == "Enemy")
+                {
+                    collision.gameObject.GetComponent<PlayerController>().takeDamage(1);
+                    collision.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
