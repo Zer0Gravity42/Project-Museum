24da8f5d223d8ff2ed4cf5d5f70fd1e21c716eff
Luke <lukewwhited@gmail.com>
2024-10-01 16:08:47 -0400

    Door functional

--- Assets/_Scripts/KeyController.cs
+++ Assets/_Scripts/KeyController.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorController : MonoBehaviour
+public class KeyController : MonoBehaviour
 {
-    
-    [SerializeField] private GameObject key;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +16,15 @@ public class DoorController : MonoBehaviour
         
     }
     
-    //On collision
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Player checking if door can be unlocked. Key Status = " + key);
         //if player collides with door
         if (collision.gameObject.tag == "Player")
         {
-            //if key is null, destroy self
-            if (key == null)
-            {
-                Destroy(gameObject);
-            }
+            //Destroy Key
+            Debug.Log("Player picked up key");
+            Destroy(this.gameObject);
         }
     }
+    
 }
