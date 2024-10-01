d551df27e63f732b05dad8d671e1654bcb244152
Aleksey Yereshchenko <aleshayy@verizon.net>
2024-09-30 22:54:12 -0400

    pushing dash on space with after image effect.

---
+++ Assets/_Scripts/AfterImageFade.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AfterImageFade : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private float alpha;
+    private float alphaDecay = 1f; //rate at which the alpha decays
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        alpha = sr.color.a;
+    }
+
+    void Update()
+    {
+        alpha -= alphaDecay * Time.deltaTime;
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, alpha);  // Keep RGB values, update alpha
+        if (alpha <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -12,14 +12,26 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     private int health = 3;
     private GameObject currentInteraction = null; //This controls our interaction system, basically what the interactable object we are currently in contact with
-    
-    
+
+
+    #region Dash Variables
+    [SerializeField] private float dashSpeed = 10f;
+    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] private float dashCooldown = 1f;  //time before dash can be used again
+    private bool isDashing = false;
+    private float lastDashTime; //timestamp for last dash
+
+    private SpriteRenderer spriteRenderer;
+    public float afterImageInterval = 0.05f;
+    private float afterImageTimer = 0f;
+    #endregion
+
     // Start is called before the first frame update
     void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
         rb.interpolation = RigidbodyInterpolation2D.Interpolate;
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,6 +40,12 @@ public class PlayerController : MonoBehaviour
         ProcessInputs();
         UpdateAnimation();
         ProcessInteracts(); //interaction system
+
+        // Handle Dash input
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= lastDashTime + dashCooldown)
+        {
+            StartCoroutine(Dash());
+        }
     }
     
     void FixedUpdate()
@@ -56,8 +74,54 @@ public class PlayerController : MonoBehaviour
     
     void Move()
     {
+        if (isDashing)
+            return; // Skip movement during dash
         rb.velocity = new Vector2(movementDirection.x * movementSpeed, movementDirection.y * movementSpeed);
     }
+
+    #region Dash functions
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        lastDashTime = Time.time;
+        Vector2 dashDirection = movementDirection;
+
+        rb.velocity = dashDirection * dashSpeed;
+
+        float dashTime = 0f;
+        afterImageTimer = 0f;
+        while (dashTime < dashDuration)
+        {
+            dashTime += Time.deltaTime;
+            afterImageTimer += Time.deltaTime;
+            if (afterImageTimer >= afterImageInterval)
+            {
+                CreateAfterImage();
+                afterImageTimer = 0f;
+            }
+            yield return null;
+        }
+
+        isDashing = false;
+        rb.velocity = Vector2.zero;
+    }
+
+    void CreateAfterImage()
+    {
+        GameObject afterImage = new GameObject("AfterImage");
+        SpriteRenderer sr = afterImage.AddComponent<SpriteRenderer>();
+        sr.sprite = spriteRenderer.sprite;
+        sr.color = new Color(1f, 1f, 0f, 0.5f); // Semi-transparent
+        sr.sortingLayerID = spriteRenderer.sortingLayerID;
+        sr.sortingOrder = spriteRenderer.sortingOrder;
+
+        afterImage.transform.position = transform.position;
+        afterImage.transform.localScale = transform.localScale;
+        afterImage.transform.rotation = transform.rotation;
+
+        afterImage.AddComponent<AfterImageFade>();
+    }
+    #endregion
 
     void UpdateAnimation()
     {
